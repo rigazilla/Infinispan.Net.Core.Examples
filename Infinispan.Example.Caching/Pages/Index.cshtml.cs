4a9a7d81d45907f15dcf587469ca13524bf7469c
Vittorio Rigamonti <vrigamon@redhat.com>
2022-05-19 16:21:26 +0200

    example for session. User code added.

--- Infinispan.Example.Caching/Pages/Index.cshtml.cs
+++ Infinispan.Example.Caching/Pages/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
 
 namespace Infinispan.Example.Caching.Pages
 {
@@ -17,8 +18,26 @@ namespace Infinispan.Example.Caching.Pages
             _logger = logger;
         }
 
+        public const string SessionKeyName = "_Name";
+        public const string SessionKeyAge = "_Age";
+        public const string SessionKeyFirstAccess = "_FirstAccess";
+        public static Random RndSource = new Random();
+        public string DataSource { get; private set; }
         public void OnGet()
         {
+
+            // Requires: using Microsoft.AspNetCore.Http;
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString(SessionKeyName)))
+            {
+                DataSource = "Computed";
+                HttpContext.Session.SetString(SessionKeyName, "Mickey");
+                HttpContext.Session.SetInt32(SessionKeyAge, RndSource.Next(100));
+                HttpContext.Session.SetString(SessionKeyFirstAccess, DateTime.Now.ToString());
+            }
+            else
+            {
+                DataSource = "Cache";
+            }
 
         }
     }
