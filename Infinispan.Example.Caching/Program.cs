480d4b6e9dd3ffc8531d4d0ac992ad8cc6a67181
Vittorio Rigamonti <vrigamon@redhat.com>
2022-06-06 09:52:20 +0200

    configure Infinispan aa service

--- Infinispan.Example.Caching/Program.cs
+++ Infinispan.Example.Caching/Program.cs
@@ -1,6 +1,23 @@
+using Infinispan.Hotrod.Core;
+using Infinispan.Hotrod.Caching.Distributed;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddInfinispanCache(options =>
+{
+    InfinispanDG cluster = new InfinispanDG();
+    cluster.AddHost("127.0.0.1");
+    options.Cluster = cluster;
+    options.CacheName = "default";
+});
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(10);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
@@ -19,6 +36,7 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+app.UseSession();
 
 app.MapRazorPages();
 
