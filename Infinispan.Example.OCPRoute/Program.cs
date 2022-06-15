e6188c3b7922269153f44335057f0b5420b18ada
Vittorio Rigamonti <vrigamon@redhat.com>
2022-06-15 18:53:41 +0200

    OCP/Route example

---
+++ Infinispan.Example.OCPRoute/Program.cs
@@ -0,0 +1,32 @@
+﻿using Infinispan.Hotrod.Core;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var myKey = "myKey";
+/// [Create a cluster object]
+InfinispanDG dg = new InfinispanDG();
+// Use a non-authenticated non-encrypted cluster;
+dg.UseTLS = true;
+dg.AuthMech = "DIGEST-MD5";
+dg.User = "developer";
+dg.Password = "F9nZlJjamlvz99zY";
+dg.Domain = "infinispan";
+dg.ServiceName = "example-infinispan-external-perfs.apps.vrigamon0531.lab.pnq2.cee.redhat.com";
+//dg.AddHost("worker-0.vrigamon0531.lab.pnq2.cee.redhat.com", 31726);
+dg.AddHost("example-infinispan-external-perfs.apps.vrigamon0531.lab.pnq2.cee.redhat.com", 443);
+dg.ClientIntelligence = 0x01;
+/// [Create a cluster object]
+/// [Create a cache]
+var cache = dg.NewCache<string, string>(new StringMarshaller(), new StringMarshaller(), "default");
+/// [Create a cache]
+try
+/// [Application code]
+{
+    await cache.Put(myKey, "some value");
+    string result = await cache.Get(myKey);
+    Console.WriteLine("Getting my entry with key {0} from the cache. Result value is: {1}", myKey, result);
+    /// [Application code]
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
